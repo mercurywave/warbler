5a300a26c44385c5d295cd29ae67d0c88969c92d
Alex Nelson <alex.nelson@mercury-wave.com>
2025-11-15 20:17:07 -0600

    service worker

--- src/index.ts
+++ src/index.ts
@@ -64,8 +64,22 @@ function spawnNote(startRecording?: boolean) {
 }
 
 async function setup(): Promise<void> {
+    setupServiceWorker();
     await Config.LoadSettings();
     await DB.Init();
     await Route.Init();
+}
+function setupServiceWorker(){
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('./sw.js')
+                .then(registration => {
+                    console.log('SW registered: ', registration);
+                })
+                .catch(error => {
+                    console.log('SW registration failed: ', error);
+                });
+        });
+    }
 }
 window.addEventListener('popstate', () => Route.OnNavigate());
---
+++ web/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'warbler-sw-cache-v1';
+const SCRIPTS_TO_CACHE = [
+    './',
+    './index.html',
+    './styles.css',
+    './app.bundle.js',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(SCRIPTS_TO_CACHE);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
